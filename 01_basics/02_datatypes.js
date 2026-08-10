89b54071db77e3d5022d0574b0ac56ad028d8246
Akanksha-66 <akankshadixit2024@gmail.com>
2026-08-11 01:13:55 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all KS code as newer js(version)
+
+// alert( 3 + 3 )  // we are using node js, not browser
+
+console.log(3+3);  console.log("Akanksha")   // if we remove ; it will trash out error because code readability gets effected so beware of it
+// code readabiity should be high 
+// mdn, tc39.es for documentation reading tc is specifically for js only
+
+
+let name = "Akanksha"
+let age = 19
+let IsLoggedIn = false
+
+// numer => 2 to the power 53
+// bigint for large numbers
+// string => " " can use single or double quotes
+// boolean => true or false
+// null => standalone value (for empty one)
+// undefined => (declared but value not assign)
+// symbol => unique
+
+
+//object
+console.log(typeof "Akanksha");
+console.log(typeof age);
+console.log(typeof null); //nulll is a object type
+console.log(typeof undefined); // undefined
+
+
